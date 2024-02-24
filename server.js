2e6df7d4feb667712ed40cd8c3b43233d8b92ad1
Fran Panteli <francescapanteli@gmail.com>
2024-02-24 21:48:52 +0000

    This commit pushes instruction on how to launch the application.

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 let express = require('express');
 let app = express();
 
-let cors = require('cors')
-app.use(cors({optionSuccessStatus: 200}));
+let cors = require('cors');
+app.use(cors({ optionSuccessStatus: 200 }));
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/views/index.html')
-})
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
 
-app.get('/api/whoami', function(req, res) {
-  res.json({"ipaddress": req.ip, "language": req.get('Accept-Language'), "software": req.get('user-agent')});
-})
+app.get('/api/whoami', function (req, res) {
+  res.json({
+    "ipaddress": req.ip,
+    "language": req.get('Accept-Language'),
+    "software": req.get('user-agent')
+  });
+});
 
-app.use(function(req, res) {
+app.use(function (req, res) {
   res.status(404);
   res.sendFile(__dirname + '/views/error.html');
-})
+});
 
-let listener = app.listen(process.env.PORT, function() {
-  console.log('app is listening on port ' + process.env.PORT)
-})
+// Use a default port if process.env.PORT is not available
+const port = process.env.PORT || 3000;
+let listener = app.listen(port, function () {
+  console.log('app is listening on port ' + port);
+});
