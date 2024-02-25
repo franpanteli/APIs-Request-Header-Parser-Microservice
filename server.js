2b76e2a404a9a691b92014d3fe2f83f7144a971b
Fran Panteli <francescapanteli@gmail.com>
2024-02-25 13:29:04 +0000

    This commit pushes the changes in the server.js file which explain the uses of the .use method throughout the file.

--- server.js
+++ server.js
@@ -31,10 +31,62 @@ let express = require('express');
 let app = express();
 let cors = require('cors');
 
+/*
+	Uses of the .use method: 
+		-> The .use method is used twice throughout this JavaScript file
+		-> This section explains those use cases 
+		-> We are using the .use method to mount middleware functions 
+		-> These functions have access to three things: 
+			-> The request object from the client to the server
+			-> The response object from the server to the client 
+			-> The next middleware function in the request-response cycle 
+		-> They are functions which can be called in between when the client makes the request and when the 
+        server makes the response -> functions which can be used on the request and response object before 
+        the transfer of information is fully completed 
+		
+	app.use(cors({ optionSuccessStatus: 200 }));
+		-> We are using cors middleware functions in the Express application 
+		-> The argument in this is a configuration object which tells the application how CORS should behave 
+		-> We are telling the CORS middleware that the preflight request is successful if the HTTP response status 
+        code is 200
+		-> This signals that the request is safe to send 
+		-> We are using the CORS middleware in the Express application -> telling it how to handle preflight requests 
+        (that a status code of 200 is considered a successful response)
 
+	app.use(express.static('public'));
+		-> We are mounting another middleware function in Express 
+			-> This is why we are using the .use method 
+			-> This is the second time this JavaScript file uses it 
+		-> We have embedded the use of a .static method inside one of these
+		-> This is a built-in Express middleware function 
+		-> We are looking in the `public` directory in the server and sending static files as response objects back to 
+      the client which made the call 
+		-> `app` is the variable which stores the instance of the Express app we previously defined 
+		-> Using the .use method on this sets up middleware for it 
+		-> Then we are using the .static method to serve the client the files in the `public` directory stored on the server   	 
+		-> The client requests a static file -> this then tells Express to look in the `public` directory in that server for 
+      the static file to serve 
+*/
 
 app.use(cors({ optionSuccessStatus: 200 }));
 app.use(express.static('public'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.get('/', function (req, res) {
